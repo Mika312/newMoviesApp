a9cc6dcbdd9cd5918e20eec0fdaefe7f3d67e796
peltier <michaelpeltier31284@gmail.com>
2020-05-11 15:07:45 +0200

    first commit

---
+++ src/containers/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchBar from "../components/search-bar";
+
+const App = () => (
+  <div>
+    <SearchBar />
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware } from "redux";
 
-import App from "./components/app";
-import reducers from "./reducers";
+import App from "./containers/app";
+// import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const API_KEY = "dcb8af96b8ac2447c12f8ea6e5dcb186";
 
 ReactDOM.render(
-  <Provider
-    store={createStoreWithMiddleware(
-      reducers,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}
-  >
-    <App />
-  </Provider>,
+  // <Provider
+  //   store={createStoreWithMiddleware(
+  //     reducers,
+  //     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  //       window.__REDUX_DEVTOOLS_EXTENSION__()
+  //   )}
+  // >
+  <App />,
+  // </Provider>,
   document.querySelector(".container")
 );
